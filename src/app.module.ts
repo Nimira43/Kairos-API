47c2aa5193b1d48573aece3d5475fa9028deac93
Nimira43 <n.rabone72@aol.com>
2025-08-05 11:37:37 +0100

    Generated Logger service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TestService } from './test/test.service'
 import { MessageFormatterService } from './message-formatter/message-formatter.service'
+import { LoggerService } from './logger/logger.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, TestService, MessageFormatterService],
+  providers: [AppService, TestService, MessageFormatterService, LoggerService],
 })
 export class AppModule {}
