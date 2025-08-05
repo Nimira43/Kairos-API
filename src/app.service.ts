ab175206d4883ab1e12de45e455133c3266350ca
Nimira43 <n.rabone72@aol.com>
2025-08-05 11:15:20 +0100

    Constructor in AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common'
+import { TestService } from './test/test.service'
 
-@Injectable()
+
 export class AppService {
+  constructor(
+    private readonly testService: TestService
+  ) {}
   getHello(): string {
     return 'Kairos API.'
   }
