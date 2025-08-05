c9f52c705f0acefe4b73d44e75c99d28030383f5
Nimira43 <n.rabone72@aol.com>
2025-08-05 11:48:02 +0100

    Creating loggerService in AppService and using it inside getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { TestService } from './test/test.service'
+import { LoggerService } from './logger/logger.service'
 
 @Injectable()
 export class AppService {
   constructor(
-    private readonly testService: TestService
+    private readonly testService: TestService,
+    private readonly loggerService: LoggerService,
   ) {}
+
   getHello(): string {
-    return `Kairos API - ${this.testService.work()}`
+    return this.loggerService.log(`Kairos API - ${this.testService.work()}`)
   }
 }
