d31495805d2f15c094b70a19e6e353b2166a2158
Nimira43 <n.rabone72@aol.com>
2025-08-05 11:29:05 +0100

    Created MessageFormatter

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TestService } from './test/test.service'
+import { MessageFormatterService } from './message-formatter/message-formatter.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, TestService],
+  providers: [AppService, TestService, MessageFormatterService],
 })
 export class AppModule {}
