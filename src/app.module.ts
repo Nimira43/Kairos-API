81acffd8501d2f1716dcb48d905ed40dd7553234
Nimira43 <n.rabone72@aol.com>
2025-08-05 11:12:12 +0100

    Create .http again and sent simple request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { TestService } from './test/test.service';
+import { TestService } from './test/test.service'
 
 @Module({
   imports: [],
