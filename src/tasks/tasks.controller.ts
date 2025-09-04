910257d9775d04b752e2a4bdc8195478b4f2ccf8
Nimira43 <n.rabone72@aol.com>
2025-09-04 22:57:55 +0100

    Updating findAll in TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Param } from '@nestjs/common'
 import { TasksService } from './tasks.service'
+import { ITask } from './task.model'
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  public findAll(): string[] {
-    return ['A', 'B']
+  public findAll(): ITask[] {
+    return this.tasksService.findAll()
   }
 
   @Get('/:id')
