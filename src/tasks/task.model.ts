3397852cb3f92538c9c60386f8a86aee2305bca7
Nimira43 <n.rabone72@aol.com>
2025-09-04 22:48:43 +0100

    Completed ITask interface

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -2,10 +2,11 @@ export interface ITask {
   id: string
   title: string
   description: string
+  status: TaskStatus
 }
 
 export enum TaskStatus {
-  OPEN,
-  IN_PROGRESS,
-  DONE
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE='DONE',
 }
