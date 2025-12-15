584c9ac83fb587990653d45d8a1117d235638449
Nimira43 <n.rabone72@aol.com>
2025-12-15 23:24:07 +0000

    Filled out constructor.

--- src/tasks/exceptions/wrong-task-status.exception.ts
+++ src/tasks/exceptions/wrong-task-status.exception.ts
@@ -1,1 +1,6 @@
-export class WrongTaskStatusException extends Error {}
+export class WrongTaskStatusException extends Error {
+  constructor() {
+    super('Wrong task status transition.')
+    this.name = 'WrongTaskStatusException'
+  }
+}
