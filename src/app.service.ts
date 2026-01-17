0ca685e7ca9bcb604d2cad1ac78c4e3b9baca9ce
Nimira43 <n.rabone72@aol.com>
2026-01-17 02:00:04 +0000

    Config types set up. Also creating the Postgres docker container.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@nestjs/common'
 import { TestService } from './tester/tester.service'
 import { LoggerService } from './logger/logger.service'
 import { ConfigService } from '@nestjs/config'
+import { ConfigType } from './config/config.types'
+import { AppConfig } from './config/app.config'
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly testService: TestService,
     private readonly loggerService: LoggerService,
-    private readonly configService: ConfigService,
+    private readonly configService: ConfigService<ConfigType>,
   ) {}
 
   getHello(): string {
-    const prefix = this.configService.get<string>('app.messagePrefix')
+    const prefix = this.configService.get<AppConfig>('app')?.messagePrefix
     return this.loggerService.log(
-      `${prefix} Kairos API - ${this.testService.work()}`
+      `${prefix} Kairos API - ${this.testService.work()}`,
     )
   }
 }
